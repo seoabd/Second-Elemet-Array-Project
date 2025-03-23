2eb3aff2cfe7466087c68bba8066d6a28043f941
seoabd <abdulisead@icloud.com>
2025-03-24 00:05:07 +0100

    initial commit

---
+++ ArraySecondElement.js
@@ -0,0 +1,9 @@
+let flowers = ['rose', 'tulip', 'sunflower', 'daisy', 'lily', 'lilac', 'orchid'];
+console.log(flowers[1]);
+
+/*Second Element
+
+
+We've created an array of flowers that contains flower names. Print the second element of this array to the console.
+
+💡 Remember that element indexes start at 0.*/
